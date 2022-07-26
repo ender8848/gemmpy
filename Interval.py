53d5f86dcaf51cc61d08e891085316ec5fa76661
hao <853789228@qq.com>
2022-07-27 00:11:37 +0800

    better comments in Interval.py class

--- Interval.py
+++ Interval.py
@@ -80,6 +80,8 @@ class Interval():
             other = Interval(other)
         return self.lower == other.lower and self.upper == other.upper
     
+
+# inline test for Interval class, print is necessary to check soundness level
 if __name__ == '__main__':
     # add test
     a = Interval(1,2)
@@ -109,7 +111,7 @@ if __name__ == '__main__':
     a /= b
     print(f"a/=b:{a}")
 
-    # test vectorize
+    # test vectorization
     a = np.array([[Interval(1,1), Interval(1,1)], [Interval(1,1), Interval(1,1)]], dtype = Interval)
     b = np.array([[Interval(1,1), Interval(1,1)], [Interval(1,1), Interval(1,1)]], dtype = Interval)
     c = a @ b
@@ -122,7 +124,7 @@ if __name__ == '__main__':
 
     # test soundness
     a = np.array([[Interval(1,1), Interval(1,1)], [Interval(1,1), Interval(1,1)]]) # no need to specify dtype, python will veiw it as object
-    for i in range (45): # propogate error 2^45 times (estimated)
+    for i in range (45): # propagate error 2^45 times (estimated)
         a = a @ a / 2
     print("array a:")
     print_2d_array(a)
@@ -132,25 +134,4 @@ if __name__ == '__main__':
     a = np.ones((3,4))
     print(a.dtype)
     a = np.ones((3,4)).astype(Interval)
-    print(a.dtype) # 直接把int变成了object，因为不能识别Interval类
-
-
-    
-
-    # 好吧就这个思路
-    # 定义好interval的operator overloading，直接用np初始化
-    # 支持对整个array直接+，*，@
-    # 但是不能用一些方便的初始化（或者批量操作），例如ones，zeros，eye，
-    # 因为这种做法相当于先用默认类型（例如float32）创建一个数组，在astype进行类型转换，但是astype的时候只会变成object，而不是Interval
-    # 因此这个数组的创建，要么靠自己输入，要么靠一个自定义函数来转换
-    # vectorize之后，numpy的的接口能直接算是因为@只用到一些__add__等接口，而这些是是基类object自带的，
-    # 由于python用的都是dynamic binding，Interval类只是override了一下，因此能被直接调用。
-    # 最后还需要2个快捷的函数，一个是由float/int/double数组转成interval类型的ndarray
-    # 另一个是由interval的ndarray快速取得upper和lower数组
-
-
-    # 关于最终的计算，差不多这样
-    # if (!interval) 直接用python默认接口就行
-    # else if (cpu) 计算直接代理给np，转换用自己的接口
-    # else if (gpu) 计算和转换用自己的接口
-    # 这种逻辑最好用积累和子类实现，也就是说还是得定义好自己的接口，不然比较麻烦
+    print(a.dtype) # object class, which means np does not recognize it as Interval
